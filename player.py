8002eba98acdb930007134ac716458f52be69cb2
chbndrhnns <chbndrhnns@users.noreply.github.com>
2023-11-23 14:36:08 +0100

    Should keep raising

--- player.py
+++ player.py
@@ -1,19 +1,36 @@
-import dataclasses
+from pydantic import BaseModel
 
 
-@dataclasses.dataclass
-class GameState:
+class Player1(BaseModel):
+    name: str
+    bet: int
+
+
+class Game(BaseModel):
     current_buy_in: int
+    minimum_raise: int
+    players: list[Player1]
+    in_action: int
+    small_blind: int
+
+    def raise_amount(self):
+        minimum_raise = self.current_buy_in - self.players[
+            self.in_action].bet + self.minimum_raise
+        return minimum_raise + self.small_blind
+
+    def fold(self):
+        ...
+
+    def check(self):
+        ...
 
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        # parse game_state
-        print(game_state)
-        return 1
+        game = Game.model_validate(game_state)
+        return game.raise_amount()
 
     def showdown(self, game_state):
         pass
-
---
+++ test_game_state.py
@@ -0,0 +1,27 @@
+from player import Game
+
+data = {'tournament_id': '655e6ea29c97ef0002da0843',
+        'game_id': '655f51fb5485f700028038ee', 'round': 7, 'players': [
+        {'name': 'Charming Rabbit', 'stack': 986, 'status': 'active', 'bet': 0,
+         'hole_cards': [{'rank': 'Q', 'suit': 'hearts'},
+                        {'rank': '2', 'suit': 'spades'}], 'time_used': 1983241,
+         'version': 'Default Python folding player', 'id': 0},
+        {'name': 'PokerGPT', 'stack': 2024, 'status': 'active', 'bet': 2,
+         'time_used': 1994034, 'version': '5 with a test', 'id': 1},
+        {'name': 'Green Rabbits', 'stack': 0, 'status': 'out', 'bet': 0,
+         'time_used': 282888, 'version': '2 - with sysout', 'id': 2},
+        {'name': 'The Semiprofessionals', 'stack': 984, 'status': 'active', 'bet': 4,
+         'time_used': 1997195, 'version': 'Kotlin Player 0.0.1', 'id': 3}],
+        'small_blind': 2, 'big_blind': 4, 'orbits': 1, 'dealer': 0,
+        'community_cards': [], 'current_buy_in': 4, 'pot': 6, 'in_action': 0,
+        'minimum_raise': 2, 'bet_index': 3}
+
+
+def test_game_state():
+    actual = Game.model_validate(data)
+    assert actual.current_buy_in == 4
+
+
+def test_minimum_raise_amount():
+    actual = Game.model_validate(data)
+    assert actual.raise_amount() == 8
