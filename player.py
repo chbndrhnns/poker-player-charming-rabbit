9921e005c744b3c766ae6845bc4266b9883665c1
chbndrhnns <chbndrhnns@users.noreply.github.com>
2023-11-23 14:21:21 +0100

    Log game state

--- player.py
+++ player.py
@@ -1,8 +1,17 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class GameState:
+    current_buy_in: int
+
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        # parse game_state
+        print(game_state)
         return 1
 
     def showdown(self, game_state):
