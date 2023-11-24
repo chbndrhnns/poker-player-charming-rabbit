f4947aaca32823f6b369786ce74a7b6bb10bdb62
Tamas Hajdu <tamas.hajdu@workday.com>
2023-11-24 13:38:24 +0100

    v4 just fold

--- player.py
+++ player.py
@@ -6,8 +6,9 @@ from pydantic import BaseModel
 
 UPPER_BET_THRESHOLD = 101
 
+
 #
-#wait out and don't raise while others are fighting
+# wait out and don't raise while others are fighting
 
 
 class Player1(BaseModel):
@@ -39,26 +40,28 @@ class Game(BaseModel):
 
 
 class Player:
-    VERSION = "v 3 rank API call"
+    VERSION = "v 4 just return 0 and fold"
 
     def call_ranking(self):
-        cards = [{"rank":"5","suit":"diamonds"},
-    {"rank":"6","suit":"diamonds"},
-    {"rank":"7","suit":"diamonds"},
-    {"rank":"7","suit":"spades"},
-    {"rank":"8","suit":"diamonds"},
-    {"rank":"9","suit":"diamonds"}
-]
+        cards = [{"rank": "5", "suit": "diamonds"},
+                 {"rank": "6", "suit": "diamonds"},
+                 {"rank": "7", "suit": "diamonds"},
+                 {"rank": "7", "suit": "spades"},
+                 {"rank": "8", "suit": "diamonds"},
+                 {"rank": "9", "suit": "diamonds"}
+                 ]
         data = f'cards={json.dumps(cards)}'
-        response = requests.post("http://rainman.leanpoker.org/rank", data=data, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        response = requests.post("http://rainman.leanpoker.org/rank", data=data,
+                                 headers={'Content-Type': 'application/x-www-form-urlencoded'})
         return response.json()
 
     def betRequest(self, game_state):
         print(f"version: {self.VERSION} ")
         print(game_state)
-        game = Game.model_validate(game_state)
-        our_data = game.get_our_data()
-        return our_data.stack
+        return 0
+        #game = Game.model_validate(game_state)
+        #our_data = game.get_our_data()
+        #return our_data.stack
 
     def showdown(self, game_state):
         pass
