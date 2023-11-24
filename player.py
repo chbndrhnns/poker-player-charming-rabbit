6853b14f223c3a638002623380ee13212c3294c4
chbndrhnns <chbndrhnns@users.noreply.github.com>
2023-11-24 14:07:48 +0100

    format

--- player.py
+++ player.py
@@ -1,53 +1,4 @@
-import json
-import logging
-from typing import Optional
-
-import requests
-from pydantic import BaseModel
-
-UPPER_BET_THRESHOLD = 101
-
-
-#
-# wait out and don't raise while others are fighting
-class Card(BaseModel):
-    rank: str
-    suit: str
-
-
-class Player1(BaseModel):
-    name: str
-    bet: int
-    stack: int
-    hole_cards: Optional[str] #Optional[list[Card]]
-
-
-class Game(BaseModel):
-    current_buy_in: int
-    minimum_raise: int
-    players: list[Player1]
-    in_action: int
-    small_blind: int
-
-    def raise_amount(self):
-        return 10000
-
-    def fold(self):
-        ...
-
-    def check(self):
-        ...
-
-    def get_our_data(self):
-        for player in self.players:
-            if player.name == 'Charming Rabbit':
-                return player
-
-
-
-
-class Player:
-    data = {'tournament_id': '655e6ea29c97ef0002da0843',
+example = {'tournament_id': '655e6ea29c97ef0002da0843',
             'game_id': '655f51fb5485f700028038ee', 'round': 7, 'players': [
             {'name': 'Charming Rabbit', 'stack': 986, 'status': 'active', 'bet': 0,
              'hole_cards': [{'rank': 'Q', 'suit': 'hearts'},
@@ -63,11 +14,8 @@ class Player:
             'community_cards': [], 'current_buy_in': 4, 'pot': 6, 'in_action': 0,
             'minimum_raise': 2, 'bet_index': 3}
 
-    def get_our_data(self):
-        for player in self.players:
-            if player.name == 'Charming Rabbit':
-                return player
 
+class Player:
     VERSION = "v 5 basic strategy"
 
     def has_pair(self, hole_cards):
